bfac1e5e109e83b542081bc66d024d0f35abfb1a
knight42 <anonymousknight96@gmail.com>
2017-05-13 15:37:40 +0800

    Add CMakeLists

---
+++ run_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HERE=$(realpath "$0")
+HERE=$(dirname "$HERE")
+name="$1"
+prog="$HERE/build/zitp"
+p="$HERE/tests/$name"
+temp=$(mktemp)
+trap 'rm -f $temp' EXIT
+
+"$prog" -i "$p/input.txt" -p "$p/program.txt" -o "$temp" >/dev/null
+
+if ! diff -q "$p/output.expected" "$temp" >/dev/null
+then
+    echo >&2 "Failed: $name"
+    exit 1
+fi
+
+exit 0
